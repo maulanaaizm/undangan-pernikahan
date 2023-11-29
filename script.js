c3f8c2d6a2bbc753652bb6f38b845759ecdaa7a1
maulanaaizm <maulanaaizm4@gmail.com>
2023-11-29 14:27:33 +0700

    bug fix countdown

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector("#nav-menu");
-
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("hamburger-active");
-  navMenu.classList.toggle("hidden");
-});
-
-//
-window.addEventListener("click", function (e) {
-  if (e.target != hamburger && e.target != navMenu) {
-    hamburger.classList.remove("hamburger-active");
-    navMenu.classList.add("hidden");
-  }
-});
-
-window.onscroll = function () {
-  const nav = document.querySelector(".navbar");
-  const fixedNav = nav.offsetTop;
-  //   const toTop = document.querySelector("#to-top");
-
-  if (window.pageYOffset > fixedNav) {
-    nav.classList.add("navbar-fixed");
-    // toTop.classList.remove("hidden");
-    // toTop.classList.add("flex");
-  } else {
-    nav.classList.remove("navbar-fixed");
-    // toTop.classList.remove("flex");
-    // toTop.classList.add("hidden");
-  }
+// Loading Page
+const refreshPage = () => {
+  location.reload();
 };
+const playLoader = () => {
+  let loader = document.querySelector(".load-wrapper");
+  let bodyLoad = document.querySelector("body");
+  loader.style.display = "none";
+  bodyLoad.classList.remove("overflow-hidden");
+};
+const stopLoader = () => {
+  let loader = document.querySelector(".load-wrapper");
+  loader.style.display = "none";
+};
+window.addEventListener("load", () => {
+  let bodyLoad = document.querySelector("body");
+  bodyLoad.classList.add("overflow-hidden");
+  setTimeout(playLoader, 2500);
+});
+
+// Navbar
+const navbar = document.querySelector("nav");
+window.addEventListener("scroll", function () {
+  if (this.window.scrollY > 50) {
+    navbar.classList.remove("hidden");
+  } else {
+    navbar.classList.add("hidden");
+  }
+});
+
+// Simply Countdown
+simplyCountdown(".simply-countdown", {
+  year: 2024, // required
+  month: 6, // required
+  day: 28, // required
+  hours: 0, // Default is 0 [0-23] integer
+
+  words: {
+    //words displayed into the countdown
+    days: { singular: "day", plural: "Days" },
+    hours: { singular: "hour", plural: "Hours" },
+    minutes: { singular: "minute", plural: "Minutes" },
+    seconds: { singular: "second", plural: "Seconds" },
+  },
+});
